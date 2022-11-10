00834690ba3a52c3f5f9d62bcf3c1fe492d95b3a
ChisomOkonkwoC <chisom.okonkwo@decagon.dev>
2022-11-10 14:25:56 +0100

    completed the car rental app functions

---
+++ RentalCarCore/Dtos/Mapping/UserMappings.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using RentalCarInfrastructure.Models;
+using RentalCarCore.Dtos.Response;
+using RentalCarCore.Dtos.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalCarCore.Dtos.Mapping
+{
+    public class UserMappings : Profile
+    {
+        public UserMappings()
+        {
+            CreateMap<User, UserResponseDto>().ReverseMap();
+            CreateMap<RegistrationDto, User>().ReverseMap();
+            CreateMap<Trip, TripsDTO>().ReverseMap();
+            CreateMap<Rating, RatingDto>().ReverseMap();
+            CreateMap<Comment, CommentDto>().ReverseMap();
+        }
+    }
+}
---
+++ RentalCarInfrastructure/ModelMail/MailService.cs
@@ -0,0 +1,62 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace RentalCarInfrastructure.ModelMail
+{
+    public class MailService : IMailService
+    {
+        private readonly MailSettings _mailSettings;
+        public MailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
+        public async Task SendEmailAsync(MailRequest mailRequest)
+        {
+            using var email = new MimeMessage()
+            {
+                Sender = MailboxAddress.Parse(_mailSettings.Mail),
+                Subject = mailRequest.Subject,
+            };
+
+            email.To.Add(MailboxAddress.Parse(mailRequest.ToEmail));
+            var builder = new BodyBuilder();
+
+            if (mailRequest.Attachments != null)
+            {
+                byte[] fileBytes;
+                foreach (var file in mailRequest.Attachments)
+                {
+                    if (file.Length > 0)
+                    {
+                        using (var stream = new MemoryStream())
+                        {
+                            file.CopyTo(stream);
+                            fileBytes = stream.ToArray();
+                        }
+                        builder.Attachments.Add(file.FileName, fileBytes, ContentType.Parse(file.ContentType));
+                    }
+                }
+            }
+
+            builder.HtmlBody = mailRequest.Body;
+            email.Body = builder.ToMessageBody();
+            using var smtp = new SmtpClient();
+            smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+            await smtp.SendAsync(email);
+            smtp.Disconnect(true);
+        }
+        public string GetEmailTemplate(string templateName)
+        {
+            var baseDir = Directory.GetCurrentDirectory();
+            string folderName = "/StaticFiles/";
+            var path = Path.Combine(baseDir + folderName, templateName);
+            return File.ReadAllText(path);
+        }
+    }
+}
